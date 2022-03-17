3719b8c53aac0c7ef7b6fb1d641a9ce04a88166f
Paulo Roberto <paulovilarjnr@gmail.com>
2022-03-17 19:39:04 -0300

    Repository and Service created for categoria

---
+++ src/main/java/com/mega/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.mega.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mega.cursomc.domain.Categoria;
+import com.mega.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
