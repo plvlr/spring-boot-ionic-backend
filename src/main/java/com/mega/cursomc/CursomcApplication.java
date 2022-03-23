c7272f0e8121a408357a6bcfd471c8ab815daa20
Paulo Roberto <paulovilarjnr@gmail.com>
2022-03-23 19:24:06 -0300

    product and association many to many

--- src/main/java/com/mega/cursomc/CursomcApplication.java
+++ src/main/java/com/mega/cursomc/CursomcApplication.java
@@ -43,6 +43,7 @@ public class CursomcApplication implements CommandLineRunner {
 			
 			categoriaRepository.saveAll(Arrays.asList(cat1, cat2));			
 			produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+			// teste
 	}
 
 }
